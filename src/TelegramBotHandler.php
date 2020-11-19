8b1be02f71b34d79aa1611d9ef4657986eb41b4d
Mahdi Bagheri <mb.programmer@yahoo.com>
2020-11-19 20:30:24 +0330

    some changes for self host proxy in telegram

--- src/TelegramBotHandler.php
+++ src/TelegramBotHandler.php
@@ -8,83 +8,86 @@ use Monolog\Logger;
 
 class TelegramBotHandler extends AbstractProcessingHandler
 {
-	/**
-	 * bot api url
-	 * @var string
-	 */
-	private $botApi;
+    /**
+     * bot api url
+     * @var string
+     */
+    private $botApi;
 
-	/**
-	 * Telegram bot access token provided by BotFather.
-	 * Create telegram bot with https://telegram.me/BotFather and use access token from it.
-	 * @var string
-	 */
-	private $token;
+    /**
+     * Telegram bot access token provided by BotFather.
+     * Create telegram bot with https://telegram.me/BotFather and use access token from it.
+     * @var string
+     */
+    private $token;
 
-	/**
-	 * if telegram is blocked in your region you can use proxy
-	 * @var null
-	 */
-	private $proxy;
+    /**
+     * if telegram is blocked in your region you can use proxy
+     * @var null
+     */
+    private $proxy;
 
-	/**
-	 * Telegram channel name.
-	 * Since to start with '@' symbol as prefix.
-	 * @var string
-	 */
-	private $chatId;
+    /**
+     * Telegram channel name.
+     * Since to start with '@' symbol as prefix.
+     * @var string
+     */
+    private $chatId;
 
-	/**
-	 * @param string $token Telegram bot access token provided by BotFather
-	 * @param string $channel Telegram channel name
-	 * @inheritDoc
-	 */
-	public function __construct(string $token, string $chat_id, $level = Logger::DEBUG, bool $bubble = true, $bot_api = 'https://api.telegram.org/bot', $proxy = null)
-	{
-		parent::__construct($level, $bubble);
+    /**
+     * @param string $token Telegram bot access token provided by BotFather
+     * @param string $channel Telegram channel name
+     * @inheritDoc
+     */
+    public function __construct(string $token, string $chat_id, $level = Logger::DEBUG, bool $bubble = true, $bot_api = 'https://api.telegram.org/bot', $proxy = null)
+    {
+        parent::__construct($level, $bubble);
 
-		$this->token = $token;
-		$this->botApi = $bot_api;
-		$this->chatId = $chat_id;
-		$this->level = $level;
-		$this->bubble = $bubble;
-		$this->proxy = $proxy;
-	}
+        $this->token = $token;
+        $this->botApi = $bot_api;
+        $this->chatId = $chat_id;
+        $this->level = $level;
+        $this->bubble = $bubble;
+        $this->proxy = $proxy;
+    }
 
-	/**
-	 * @inheritDoc
-	 */
-	protected function write(array $record): void
-	{
-		$this->send($record['formatted']);
-	}
+    /**
+     * @inheritDoc
+     */
+    protected function write(array $record): void
+    {
+        $this->send($record['formatted']);
+    }
 
-	/**
-	 * Send request to @link https://api.telegram.org/bot on SendMessage action.
-	 * @param string $message
-	 */
-	protected function send(string $message, $option = []): void
-	{
-		try {
-			if (!is_null($this->proxy)) {
-				$option['proxy'] = $this->proxy;
-			}
-			$httpClient = new Client($option);
+    /**
+     * Send request to @link https://api.telegram.org/bot on SendMessage action.
+     * @param string $message
+     */
+    protected function send(string $message, $option = []): void
+    {
+        try {
+            if (!is_null($this->proxy)) {
+                $option['proxy'] = $this->proxy;
+            }
+            $httpClient = new Client($option);
 
-			$url = $this->botApi . $this->token . '/SendMessage';
-			$options = [
-				'form_params' => [
-					'text' => $message,
-					'chat_id' => $this->chatId,
-					'parse_mode' => 'html',
-					'disable_web_page_preview' => true,
-				]
-			];
-			$response = $httpClient->post($url, $options);
-		} catch (\Exception $e) {
-			
-		}
-	}
+            if (strpos($this->botApi, 'https://api.telegram.org')) {
+                $url = $this->botApi . $this->token . '/SendMessage';
+            } else {
+                $url = $this->botApi;
+            }
+
+            $options = [
+                'form_params' => [
+                    'text' => $message,
+                    'chat_id' => $this->chatId,
+                    'parse_mode' => 'html',
+                    'disable_web_page_preview' => true,
+                ]
+            ];
+            $response = $httpClient->post($url, $options);
+        } catch (\Exception $e) {
+
+        }
+    }
 }
-
-
