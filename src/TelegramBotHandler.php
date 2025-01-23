5c49b7c1b5da560ed4d3843874fc38af1ff375ef
Mahdi Bagheri <mb.reckless@gmail.com>
2025-01-23 18:24:28 +0330

    feat(console): check running app in command or job and get topic from handle function

--- src/TelegramBotHandler.php
+++ src/TelegramBotHandler.php
@@ -3,18 +3,11 @@
 namespace TheCoder\MonologTelegram;
 
 use GuzzleHttp\Exception\GuzzleException;
-use Illuminate\Routing\Router;
-use Illuminate\Support\Facades\Route;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
-use ReflectionMethod;
-use TheCoder\MonologTelegram\Attributes\TopicLogInterface;
 
 class TelegramBotHandler extends AbstractProcessingHandler
 {
-
-    protected Router $router;
-
     /**
      * text parameter in sendMessage method
      * @see https://core.telegram.org/bots/api#sendmessage
@@ -25,27 +18,27 @@ class TelegramBotHandler extends AbstractProcessingHandler
      * bot api url
      * @var string
      */
-    protected $botApi;
+    protected string $botApi;
 
     /**
      * Telegram bot access token provided by BotFather.
      * Create telegram bot with https://telegram.me/BotFather and use access token from it.
      * @var string
      */
-    protected $token;
+    protected string $token;
 
     /**
      * if telegram is blocked in your region you can use proxy
      * @var null
      */
-    protected $proxy;
+    protected string|null $proxy;
 
     /**
      * Telegram channel name.
      * Since to start with '@' symbol as prefix.
      * @var string
      */
-    protected $chatId;
+    protected string|int $chatId;
 
     /**
      * If chat groups are used instead of telegram channels,
@@ -53,11 +46,11 @@ class TelegramBotHandler extends AbstractProcessingHandler
      * this configuration can be utilized.
      * @var string|null
      */
-    protected $topicId;
+    protected string|int|null $topicId;
 
-    protected $queue = null;
+    protected string|null $queue = null;
 
-    protected $topicsLevel;
+    protected TopicDetector $topicDetector;
 
     /**
      * @param string $token Telegram bot access token provided by BotFather
@@ -65,29 +58,28 @@ class TelegramBotHandler extends AbstractProcessingHandler
      * @inheritDoc
      */
     public function __construct(
-        Router  $router,
-        string  $token,
-        string  $chat_id,
-        ?string $topic_id = null,
-        ?string $queue = null,
-                $topics_level = [],
-                $level = Logger::DEBUG,
-        bool    $bubble = true,
-                $bot_api = 'https://api.telegram.org/bot',
-                $proxy = null)
+        string      $token,
+        string|int  $chat_id,
+        string|null $topic_id = null,
+        string|null $queue = null,
+        array       $topics_level = [],
+                    $level = Logger::DEBUG,
+        bool        $bubble = true,
+        string      $bot_api = 'https://api.telegram.org/bot',
+        string|null $proxy = null)
     {
         parent::__construct($level, $bubble);
 
-        $this->router = $router;
         $this->token = $token;
         $this->botApi = $bot_api;
         $this->chatId = $chat_id;
         $this->topicId = $topic_id;
         $this->queue = $queue;
-        $this->topicsLevel = $topics_level;
         $this->level = $level;
         $this->bubble = $bubble;
         $this->proxy = $proxy;
+
+        $this->topicDetector = new TopicDetector($topics_level);
     }
 
     /**
@@ -96,7 +88,8 @@ class TelegramBotHandler extends AbstractProcessingHandler
      */
     protected function write($record): void
     {
-        $topicId = $this->getTopicByAttribute($record);
+        $topicId = $this->topicDetector->getTopicByAttribute($record);
+
         $token = $record['context']['token'] ?? null;
         $chatId = $record['context']['chat_id'] ?? null;
         $topicId = $topicId ?? $record['context']['topic_id'] ?? null;
@@ -140,151 +133,4 @@ class TelegramBotHandler extends AbstractProcessingHandler
         }
     }
 
-    protected function getTopicByAttribute($record): string|null
-    {
-        if (isset($record['context']['exception'])) {
-            $trace = $record['context']['exception']->getTrace();
-
-            $commandClass = $this->getClassForCommand($trace);
-            if ($commandClass) {
-                return $this->getTopicIdByReflection($commandClass, 'handle');
-            }
-
-            $jobClass = $this->getClassForJob($trace);
-            if ($jobClass) {
-                return $this->getTopicIdByReflection($jobClass, 'handle');
-            }
-        }
-
-        return $this->getTopicByRoute();
-    }
-
-    protected function getClassForCommand(array $trace): ?string
-    {
-        if (!app()->runningInConsole()) {
-            return null;
-        }
-
-        foreach ($trace as $frame) {
-            if ($frame['function'] === 'handle' && isset($frame['class']) && str_contains($frame['class'], 'Console\Commands')) {
-                return $frame['class'];
-            }
-        }
-
-        return null;
-    }
-
-    protected function getClassForJob(array $trace): ?string
-    {
-        if (!app()->bound('queue.worker')) {
-            return null;
-        }
-
-        foreach ($trace as $frame) {
-            if ($frame['function'] === 'handle' && isset($frame['class']) && str_contains($frame['class'], 'App\Jobs')) {
-                return $frame['class'];
-            }
-        }
-
-        return null;
-    }
-
-    protected function getTopicByRoute(): string|null
-    {
-        $route = Route::current();
-        if (!$route || !isset($route->getAction()['controller'])) {
-            return null;
-        }
-
-        [$controller, $method] = explode('@', $route->getAction()['controller']);
-
-        $topicId = $this->getTopicIdByReflection($controller, $method);
-        if ($topicId === false) {
-            $topicId = $this->getTopicIdByRegex($route->getAction());
-        }
-
-        return $topicId;
-    }
-
-    protected function getTopicIdByReflection($class, $method): bool|string|null
-    {
-        try {
-            $reflectionMethod = new ReflectionMethod($class, $method);
-
-            $attributes = $reflectionMethod->getAttributes();
-
-            if ($attributes[0] !== null) {
-                /** @var TopicLogInterface $notifyException */
-                $notifyException = $attributes[0]->newInstance() ?? null;
-                return $notifyException->getTopicId($this->topicsLevel);
-            }
-
-        } catch (\Throwable $e) {
-
-        }
-        return false;
-    }
-
-    protected function getTopicIdByRegex($action)
-    {
-        try {
-            // if reflection could not get attribute use regex instead
-            [$controller, $method] = explode('@', $action['controller']);
-
-            $filePath = base_path(str_replace('App', 'app', $controller) . '.php');
-            $fileContent = file_get_contents($filePath);
-            $allAttributes = [];
-
-            // Regex to match attributes and methods
-            $regex = '/\#\[\s*(.*?)\s*\]\s*public\s*function\s*(\w+)/';
-            if (preg_match_all($regex, $fileContent, $matches, PREG_SET_ORDER)) {
-                foreach ($matches as $match) {
-                    $attributeString = $match[1];
-                    $methodName = $match[2];
-
-                    $attributes = array_map('trim', explode(',', $attributeString));
-                    foreach ($attributes as $attribute) {
-                        $attributeName = preg_replace('/\(.*/', '', $attribute);
-                        $allAttributes[$methodName][] = $attributeName;
-                    }
-                }
-            }
-
-            if (empty($allAttributes)) {
-                return null;
-            }
-
-            if (isset($allAttributes[$method][0])) {
-                foreach ($this->topicsLevel as $key => $_topicLevel) {
-                    if (str_contains($key, $allAttributes[$method][0])) {
-                        return $_topicLevel;
-                    }
-                }
-            }
-
-        } catch (\Throwable $e) {
-        }
-        return null;
-    }
-
-    public function setToken(string $token): static
-    {
-        $this->token = $token;
-
-        return $this;
-    }
-
-    public function setChatId(string $chatId): static
-    {
-        $this->chatId = $chatId;
-
-        return $this;
-    }
-
-    public function setTopicId(string $topicId): static
-    {
-        $this->topicId = $topicId;
-
-        return $this;
-    }
 }
---
+++ src/TopicDetector.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace TheCoder\MonologTelegram;
+
+use ReflectionMethod;
+use TheCoder\MonologTelegram\Attributes\TopicLogInterface;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Route;
+
+class TopicDetector
+{
+    private array $topicsLevel;
+    private mixed $exception;
+    private mixed $trace;
+
+    public function __construct(array $topicsLevel)
+    {
+        $this->topicsLevel = $topicsLevel;
+    }
+
+    public function getTopicByAttribute(mixed $record): string|null
+    {
+        if ($this->appRunningWithRequest()) {
+            return $this->getTopicByRoute();
+        }
+
+        if (isset($record['context']['exception'])) {
+            $this->exception = $record['context']['exception'];
+            $this->trace = $this->exception->getTrace();
+
+            if ($this->appRunningWithCommand()) {
+                return $this->getTopicIdByCommand();
+            }
+
+            if ($this->appRunningWithJob()) {
+                return $this->getTopicIdByJob();
+            }
+        }
+
+        return null;
+    }
+
+
+    // Route Function
+    private function appRunningWithRequest(): bool
+    {
+        return Route::current() !== null;
+    }
+
+    private function getTopicByRoute(): string|null
+    {
+        $route = Route::current();
+        if (!isset($route->getAction()['controller'])) {
+            return null;
+        }
+
+        [$controller, $method] = explode('@', $route->getAction()['controller']);
+
+        $topicId = $this->getTopicIdByReflection($controller, $method);
+        if ($topicId === false) {
+            $topicId = $this->getTopicIdByRegex($controller, $method);
+        }
+
+        return $topicId;
+    }
+
+    // Job function
+    private function appRunningWithJob(): bool
+    {
+        if (isset($e->job) || app()->bound('queue.worker')) {
+            return true;
+        }
+        return false;
+    }
+
+    private function getJobClass(): string|null
+    {
+        if (!app()->bound('queue.worker')) {
+            return null;
+        }
+
+        foreach ($this->trace as $frame) {
+            if ($frame['function'] === 'handle' && isset($frame['class']) && str_contains($frame['class'], 'App\Jobs')) {
+                return $frame['class'];
+            }
+        }
+
+        return null;
+    }
+
+    private function getTopicIdByJob(): string|int|null
+    {
+        $topicId = null;
+        $jobClass = $this->getJobClass();
+        if ($jobClass !== null) {
+            $topicId = $this->getTopicIdByReflection($jobClass, 'handle');
+            if ($topicId === false) {
+                $topicId = $this->getTopicIdByRegex($jobClass, 'handle');
+            }
+        }
+        return $topicId;
+    }
+
+    // Command function
+    private function appRunningWithCommand(): bool
+    {
+        return app()->runningInConsole();
+    }
+
+    private function getCommandClass(): string|null
+    {
+        $filePath = $this->exception->getFile();
+
+        if (str_contains($filePath, 'Console\Commands')) {
+            $appPosition = strpos($filePath, 'app');
+            if ($appPosition !== false) {
+                $appPath = substr($filePath, $appPosition);
+                return str_replace(['/', 'app', '.php'], ['\\', 'App', ''], $appPath);
+            }
+        }
+
+        foreach ($this->trace as $frame) {
+            if ($frame['function'] === 'handle' && isset($frame['class']) && str_contains($frame['class'], 'Console\Commands')) {
+                return $frame['class'];
+            }
+        }
+
+        return null;
+    }
+
+    private function getTopicIdByCommand(): string|int|null
+    {
+        $topicId = null;
+        $commandClass = $this->getCommandClass();
+        if ($commandClass !== null) {
+            $topicId = $this->getTopicIdByReflection($commandClass, 'handle');
+            if ($topicId === false) {
+                $topicId = $this->getTopicIdByRegex($commandClass, 'handle');
+            }
+        }
+        return $topicId;
+    }
+
+    // General function
+    private function getTopicIdByReflection(string $class, string $method): string|int|null|bool
+    {
+        try {
+            $reflectionMethod = new ReflectionMethod($class, $method);
+
+            $attributes = $reflectionMethod->getAttributes();
+
+            if (count($attributes) !== 0 && $attributes[0] !== null) {
+                /** @var TopicLogInterface $notifyException */
+                $notifyException = $attributes[0]->newInstance() ?? null;
+                return $notifyException->getTopicId($this->topicsLevel);
+            }
+
+        } catch (\Throwable $e) {
+
+        }
+        return false;
+    }
+
+    private function getTopicIdByRegex(string $class, string $method): string|int|null
+    {
+        try {
+
+            $filePath = base_path(str_replace('App', 'app', $class) . '.php');
+            $fileContent = file_get_contents($filePath);
+            $allAttributes = [];
+
+            // Regex to match attributes and methods
+            $regex = '/\#\[\s*(.*?)\s*\]\s*public\s*function\s*(\w+)/';
+            if (preg_match_all($regex, $fileContent, $matches, PREG_SET_ORDER)) {
+                foreach ($matches as $match) {
+                    $attributeString = $match[1];
+                    $methodName = $match[2];
+
+                    $attributes = array_map('trim', explode(',', $attributeString));
+                    foreach ($attributes as $attribute) {
+                        $attributeName = preg_replace('/\(.*/', '', $attribute);
+                        $allAttributes[$methodName][] = $attributeName;
+                    }
+                }
+            }
+
+            if (empty($allAttributes)) {
+                return null;
+            }
+
+            if (isset($allAttributes[$method][0])) {
+                foreach ($this->topicsLevel as $key => $_topicLevel) {
+                    if (str_contains($key, $allAttributes[$method][0])) {
+                        return $_topicLevel;
+                    }
+                }
+            }
+
+        } catch (\Throwable $e) {
+        }
+        return null;
+    }
+
+}
